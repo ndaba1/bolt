103ada26b22705e42f9bbbdebe770b48597dfb00
Victor Ndaba <mwangindaba@gmail.com>
2022-02-07 08:01:11 +0300

    feat(lib): added modules for the main bolt commands
    created modules for each of the boly commands to be implemented

--- src/lib/cmd/mod.rs
+++ src/lib/cmd/mod.rs
@@ -1,3 +1,8 @@
+pub mod add;
+pub mod init;
+pub mod redirect;
+pub mod summary;
+pub mod test;
 pub mod up;
 
 pub use up::up;
--- src/lib/cmd/up.rs
+++ src/lib/cmd/up.rs
@@ -1,9 +1,9 @@
-use crate::parser::Command;
 use super::super::program::Program;
-
+use super::super::utils::{get_config, read_file_tree};
+use crate::parser::Command;
+use std::path::Path;
 
 pub fn up(args: &Vec<String>, cmd: &Command) {
-
     if args.is_empty() {
         Program::output_command_help(cmd, "Missing required argument");
         return;
@@ -11,8 +11,27 @@ pub fn up(args: &Vec<String>, cmd: &Command) {
 
     let val = args[0].to_lowercase();
     let name = val.as_str();
+
     match name {
         "--help" | "-h" => Program::output_command_help(cmd, ""),
-        _ => println!("Currently running fn up with args: {:?}", args)
+        _ => {
+            let path = format!("./projects/{}", &name);
+            let results = read_file_tree(Path::new("./projects")).unwrap();
+
+            let target: Vec<_> = results
+                .iter()
+                .filter(|r| match r.to_ascii_lowercase().to_str() {
+                    Some(val) => val == name,
+                    None => false,
+                })
+                .collect();
+
+            if target.is_empty() {
+                eprintln!("Could not find target directory: {}", &name);
+                return;
+            }
+
+            get_config(Path::new(&path)).unwrap();
+        }
     };
 }
