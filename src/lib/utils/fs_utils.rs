bc2066e7915541f672e78e446dfababdae51c107
Victor Ndaba <mwangindaba@gmail.com>
2022-02-08 15:27:59 +0300

    fix(lib/utils): removed the unused chck_cfg fn
    The check config fn moved to bolt_utils and deleted from fs_utils

--- src/lib/utils/fs_utils.rs
+++ src/lib/utils/fs_utils.rs
@@ -14,23 +14,3 @@ pub fn read_file_tree(path: &Path) -> io::Result<Vec<OsString>> {
     }
     Ok(entries)
 }
-
-pub fn get_config(dir: &Path) -> io::Result<()> {
-    if dir.is_dir() {
-        let children = read_file_tree(dir)?;
-
-        let target: &Vec<_> = &children
-            .iter()
-            .filter(|c| c.to_ascii_lowercase().to_str() == Some("boltconfig.json"))
-            .collect();
-
-        if target.is_empty() {
-            println!(
-                "Could not find a boltconfig.json in dir: {:?}",
-                Path::new(dir).to_str().unwrap()
-            )
-        }
-    }
-
-    Ok(())
-}
--- src/lib/utils/mod.rs
+++ src/lib/utils/mod.rs
@@ -1,3 +1,5 @@
+mod bolt_utils;
 mod fs_utils;
 
-pub use fs_utils::{read_file_tree, get_config};
+pub use bolt_utils::{get_config, Config, Policy};
+pub use fs_utils::read_file_tree;
