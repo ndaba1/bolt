f361fce665033582c9a50c3e4a887692106b4e14
Victor Ndaba <mwangindaba@gmail.com>
2022-02-09 22:39:42 +0300

    refactor(lib/cmd): implemented some separation of concerns
    generalized and removed execute and resolve_policy function

--- src/lib/cmd/up.rs
+++ src/lib/cmd/up.rs
@@ -1,12 +1,11 @@
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::path::Path;
 
+use super::super::core::execute;
+use super::super::core::resolve_policy;
 use super::super::parser;
 use super::super::program::Program;
+use super::super::utils::Config;
 use super::super::utils::{get_config, read_file_tree};
-use super::super::utils::{Config, Policy};
-
-use colored::Colorize;
 
 pub fn up(args: &Vec<String>, cmd: &parser::Command) {
     if args.is_empty() {
@@ -62,66 +61,7 @@ fn start(cfg: Config, value: String, dir: &String) {
             execute(&cmd, &env, &full_path, true, &msg);
         }
         (None, cmd, msg) => {
-            dbg!(&msg);
             execute(&cmd, &env, &full_path, true, &msg);
         }
-    }
-}
-
-fn execute(cmd: &String, target: &str, dir: &PathBuf, wait: bool, msg: &str) {
-    let shell = if target == "windows" { "cmd" } else { "sh" };
-    let first_arg = if target == "windows" { "/C" } else { "-c" };
-
-    println!("{}", msg.green());
-
-    let mut command = Command::new(shell)
-        .arg(first_arg)
-        .arg(cmd)
-        .current_dir(dir)
-        .spawn()
-        .expect("Failed to start the application");
-
-    if wait {
-        command.wait().expect("Failed to wait on the child process");
-    }
-}
-
-fn resolve_policy(cfg: Config, val: String) -> (Option<String>, String, String) {
-    let cmd: Vec<&Policy> = cfg.policies.iter().filter(|p| p.name == val).collect();
-    let policy = &cmd.first().unwrap();
-
-    let pre_load = if policy.depends_on.is_empty() {
-        None
-    } else if policy.depends_on == "^" || policy.depends_on == "install" {
-        Some(String::from("install"))
-    } else {
-        Some(policy.depends_on.clone())
-    };
-
-    let target = &policy.map_to.value;
-
-    match target.as_str() {
-        "cli" => {
-            let target_cli = cfg.info.env.cli.unwrap().clone();
-            let target_cmd = policy.map_to.cmd.clone().unwrap();
-
-            (
-                pre_load,
-                format!("{} {}", target_cli, target_cmd),
-                "🚀 Starting your application...".to_owned(),
-            )
-        }
-        "pkg_mgr" => {
-            let pkgr = cfg.info.env.pkg_mgr;
-            let value = pkgr.value;
-            let cmd = pkgr.cmds.add;
-
-            (
-                pre_load,
-                format!("{} {}", value, cmd),
-                "➕ Installing dependencies...".to_owned(),
-            )
-        }
-        _ => (None, "".to_owned(), "".to_owned()),
     }
 }
