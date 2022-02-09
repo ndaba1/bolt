cec3e8e12125d7b7cd75d469174f41213adedab6
Victor Ndaba <mwangindaba@gmail.com>
2022-02-09 21:40:15 +0300

    feat(lib/cmd): up command, now impl the depends on prop
    Whenever a property is passed to the dependsOn field, it will be

--- src/lib/cmd/up.rs
+++ src/lib/cmd/up.rs
@@ -1,10 +1,12 @@
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+use super::super::parser;
 use super::super::program::Program;
 use super::super::utils::{get_config, read_file_tree};
 use super::super::utils::{Config, Policy};
-use crate::parser;
+
 use colored::Colorize;
-use std::path::{Path, PathBuf};
-use std::process::Command;
 
 pub fn up(args: &Vec<String>, cmd: &parser::Command) {
     if args.is_empty() {
@@ -36,49 +38,90 @@ pub fn up(args: &Vec<String>, cmd: &parser::Command) {
             }
 
             let config = get_config(&Path::new(path.as_str()));
-            start(config, &path)
+            start(config, "up".to_owned(), &path)
         }
     };
 }
 
 // Read boltconfing, get cli, get policy, execute it
-fn start(cfg: Config, dir: &String) {
-    let cmd: Vec<&Policy> = cfg
-        .policies
-        .iter()
-        .filter(|p| p.name == "up".to_owned())
-        .collect();
-
-    let root_path = std::env::current_dir().unwrap();
-    let target_cli = cfg.info.env.cli.unwrap();
-    let cli_cmd = &cmd.first().unwrap().map_to;
-
-    if target_cli.is_empty() {
-        println!("No cli present, exiting...");
-    }
-    let full_path = Path::new(&root_path).join(&dir);
-    let cmd_args = vec![target_cli, cli_cmd.clone()];
+fn start(cfg: Config, value: String, dir: &String) {
+    let env;
 
     if cfg!(target_os = "windows") {
-        execute(cmd_args, "windows", full_path)
+        env = "windows"
     } else {
-        execute(cmd_args, "not-windows", full_path)
+        env = "unix"
     };
+
+    let root_path = std::env::current_dir().unwrap();
+    let full_path = Path::new(&root_path).join(&dir);
+
+    match resolve_policy(cfg.clone(), value) {
+        (Some(val), cmd, msg) => {
+            start(cfg.clone(), val, &dir);
+            execute(&cmd, &env, &full_path, true, &msg);
+        }
+        (None, cmd, msg) => {
+            dbg!(&msg);
+            execute(&cmd, &env, &full_path, true, &msg);
+        }
+    }
 }
 
-fn execute(args: Vec<String>, target: &str, dir: PathBuf) {
+fn execute(cmd: &String, target: &str, dir: &PathBuf, wait: bool, msg: &str) {
     let shell = if target == "windows" { "cmd" } else { "sh" };
     let first_arg = if target == "windows" { "/C" } else { "-c" };
 
-    println!("{}", "🚀 Starting your application...".green());
+    println!("{}", msg.green());
 
     let mut command = Command::new(shell)
         .arg(first_arg)
-        .args(args)
+        .arg(cmd)
         .current_dir(dir)
         .spawn()
         .expect("Failed to start the application");
 
-    command.wait().expect("Failed to wait on the child process");
-    println!("Bolt exited successfully!");
+    if wait {
+        command.wait().expect("Failed to wait on the child process");
+    }
+}
+
+fn resolve_policy(cfg: Config, val: String) -> (Option<String>, String, String) {
+    let cmd: Vec<&Policy> = cfg.policies.iter().filter(|p| p.name == val).collect();
+    let policy = &cmd.first().unwrap();
+
+    let pre_load = if policy.depends_on.is_empty() {
+        None
+    } else if policy.depends_on == "^" || policy.depends_on == "install" {
+        Some(String::from("install"))
+    } else {
+        Some(policy.depends_on.clone())
+    };
+
+    let target = &policy.map_to.value;
+
+    match target.as_str() {
+        "cli" => {
+            let target_cli = cfg.info.env.cli.unwrap().clone();
+            let target_cmd = policy.map_to.cmd.clone().unwrap();
+
+            (
+                pre_load,
+                format!("{} {}", target_cli, target_cmd),
+                "🚀 Starting your application...".to_owned(),
+            )
+        }
+        "pkg_mgr" => {
+            let pkgr = cfg.info.env.pkg_mgr;
+            let value = pkgr.value;
+            let cmd = pkgr.cmds.add;
+
+            (
+                pre_load,
+                format!("{} {}", value, cmd),
+                "➕ Installing dependencies...".to_owned(),
+            )
+        }
+        _ => (None, "".to_owned(), "".to_owned()),
+    }
 }
