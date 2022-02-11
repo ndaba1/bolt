ac358647c9956ab63492fc289ebccc8863d8d287
Victor Ndaba <mwangindaba@gmail.com>
2022-02-11 17:39:39 +0300

    fix(lib/utils): the fn checks for config in root bolt.json

--- src/lib/utils/resolve_alias.rs
+++ src/lib/utils/resolve_alias.rs
@@ -4,13 +4,28 @@ use std::path::Path;
 use super::super::utils;
 
 pub fn resolve_alias(root: &str, alias: &str, results: Vec<OsString>) -> Option<String> {
+    let wp_cfg = utils::get_workspace_config();
     let target: Vec<_> = results
         .iter()
         .filter(|r| {
-            let proj_path: String = format!("{}/{}/", root, r.to_str().unwrap());
-            let config = utils::get_project_config(&Path::new(proj_path.as_str()));
+            let str_version = r.to_str().unwrap();
+            match &wp_cfg.registered {
+                Some(vector) => {
+                    if vector.contains(&str_version.to_owned()) {
+                        let proj_path: String = format!("{}/{}/", root, str_version);
+                        let config = utils::get_project_config(&Path::new(proj_path.as_str()));
 
-            config.info.alias == alias
+                        return config.info.alias == alias;
+                    }
+                    false
+                }
+                None => {
+                    let proj_path: String = format!("{}/{}/", root, str_version);
+                    let config = utils::get_project_config(&Path::new(proj_path.as_str()));
+
+                    config.info.alias == alias
+                }
+            }
         })
         .collect();
 
