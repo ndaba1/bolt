dacdb2188d6aaece48f0b8bfa2ff33be80eccda3
Victor Ndaba <mwangindaba@gmail.com>
2022-02-21 19:37:47 +0300

    refactor(lib/cmd): completed load directives functionality

---
+++ src/lib/cmd/load.rs
@@ -0,0 +1,24 @@
+use std::path::Path;
+
+use super::super::core;
+use super::super::parser;
+use super::super::program::Program;
+
+pub fn load(args: &Vec<String>, cmd: &parser::Command) {
+    if args.is_empty() {
+        // dont throw err, instead prompt for opts: run all, enter prio, choose
+        Program::output_command_help(cmd, "Missing required argument");
+        return;
+    }
+
+    let val = args[0].to_lowercase();
+    let name = val.as_str();
+
+    match name {
+        "--help" | "-h" => Program::output_command_help(cmd, ""),
+        _ => {
+            let (proj_path, _config) = core::setup_cmd(name);
+            core::load_directives(Path::new(proj_path.as_str()), true);
+        }
+    };
+}
