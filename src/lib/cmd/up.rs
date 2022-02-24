4f687c6ad3501ebc0dfb0110651a54dba1575792
Victor Ndaba <mwangindaba@gmail.com>
2022-02-24 08:46:58 +0300

    chore(lib/cmd): refactored the inputs and outputs of the up cmd

--- src/lib/cmd/up.rs
+++ src/lib/cmd/up.rs
@@ -1,48 +1,64 @@
 use std::path::Path;
 
+use colored::Colorize;
+
+use crate::core::process_args::process_args;
+
 use super::super::core;
 use super::super::parser;
 use super::super::program::Program;
 use super::super::utils::ProjectConfig;
 
-pub fn up(args: &Vec<String>, cmd: &parser::Command) {
+struct UpConfig {
+    // help: bool,
+    skip: bool,
+}
+
+pub fn up(cmd: &parser::Command, args: &Vec<String>) {
     if args.is_empty() {
         // dont throw err, instead prompt for opts: run all, enter prio, choose
         Program::output_command_help(cmd, "Missing required argument");
         return;
     }
 
-    let val = args[0].to_lowercase();
-    let name = val.as_str();
-
-    match name {
-        "--help" | "-h" => Program::output_command_help(cmd, ""),
-        _ => {
-            let (proj_path, config) = core::setup_cmd(name);
-            start(config, "up".to_owned(), &proj_path)
-        }
+    let (flags, name) = process_args(cmd, args);
+    let mut opts = UpConfig {
+        // help: false,
+        skip: false,
     };
+
+    for flag in flags {
+        match flag.as_str() {
+            "-s" | "--skip" => opts.skip = true,
+            "-h" | "--help" => {
+                Program::output_command_help(cmd, "");
+                return;
+            }
+            _ => continue,
+        }
+    }
+
+    if name.is_empty() {
+        println!("{}", "Please pass the name of the project to start".red());
+        return;
+    }
+
+    let (proj_path, config) = core::setup_cmd(&name);
+    core::load_directives(Path::new(&proj_path.as_str()), false);
+    start(config, "up".to_owned(), &proj_path)
 }
 
 fn start(cfg: ProjectConfig, value: String, dir: &String) {
-    let env;
-
-    if cfg!(target_os = "windows") {
-        env = "windows"
-    } else {
-        env = "unix"
-    };
-
     let root_path = std::env::current_dir().unwrap();
     let full_path = Path::new(&root_path).join(&dir);
 
     match core::resolve_policy(cfg.clone(), value) {
         (Some(val), cmd, msg) => {
             start(cfg.clone(), val, &dir);
-            core::execute(&cmd, &env, &full_path, true, &msg);
+            core::execute(&cmd, &full_path, true, &msg);
         }
         (None, cmd, msg) => {
-            core::execute(&cmd, &env, &full_path, true, &msg);
+            core::execute(&cmd, &full_path, true, &msg);
         }
     }
 }
