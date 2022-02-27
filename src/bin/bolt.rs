3ae43418561810d8af607b1aa13681d7ea3347ad
Victor Ndaba <mwangindaba@gmail.com>
2022-02-27 15:32:53 +0300

    feat(bin): simplified the resolve cmd functionality

--- src/bin/bolt.rs
+++ src/bin/bolt.rs
@@ -3,18 +3,15 @@ use std::env;
 use bolt::parser::Command;
 use bolt::program::Program;
 
-use bolt::cmd;
 use bolt::core;
 
 /// Creates a new instance of program and initializes it to build all commands.
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let refined_args = args[1..].to_vec();
+
     let mut program = Program::new();
     program.init();
-
-    let args: Vec<String> = env::args().collect();
-    let size = args.len();
-
-    let refined_args = args[1..size].to_vec();
 
     let config = program.parse(&refined_args);
     match config {
@@ -26,18 +23,7 @@ fn main() {
 /// Matches through all the internal commands and calls the appropriate method.
 fn resolve_cmd(cfg: &Vec<&Command>, args: &Vec<String>) {
     let command = cfg.first().unwrap();
-
-    let config: Vec<_> = command.name.split(",").collect();
-    let arr_len = args.len();
-
-    let refined_args = &args[1..arr_len].to_vec();
-    // (command.callback)(command, refined_args);
-
-    match config[0] {
-        "up" => cmd::up(command, refined_args),
-        "load" => cmd::load(command, refined_args),
-        _ => println!("Something else"),
-    }
+    (command.callback)(command, &args[1..].to_vec());
 }
 
 fn redirect_cmd(prog: &Program, args: &Vec<String>) {
