1819d3964771ba4b1e46411b0d98539bcf52b712
Victor Ndaba <mwangindaba@gmail.com>
2022-03-10 22:26:15 +0300

    chore: deleted the parser and program modules
    removed any parsing functionality since now using cmder crate

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,13 +1,5 @@
-/// This module contains implementations for the program struct and creates the actual program commands
-/// When the program starts, a new instance of the program struct is created and used to parse commands
-/// It also contains the functions for outputting general command help
-pub mod program;
-
 /// The cmd module contains the implementations of the commands, each in its own module
 pub mod cmd;
-
-/// Contains a set of utility functions used for creating a command, each function returns a mutable reference to self allowing multiple methods to be chained together
-pub mod parser;
 
 /// A set of general utilities for the program, such as reading and writing to the fs
 pub mod utils;
--- src/lib/parser/mod.rs
+++
@@ -1,4 +0,0 @@
-/// The commands module contains the implementations for mutating a command
-pub mod commands;
-
-pub use commands::Command;
