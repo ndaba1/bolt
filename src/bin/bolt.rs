a47accf81fafd56443abe79afaf3c6f7611c2d3a
Victor Ndaba <mwangindaba@gmail.com>
2022-03-13 22:17:08 +0300

    feat: added bolt test command support

--- src/bin/bolt.rs
+++ src/bin/bolt.rs
@@ -8,7 +8,7 @@ fn main() {
     program
         .version("0.1.0")
         .description("A CLI for managing multi-lingual monorepos")
-        .author("Victor M Ndaba <vndabam@gmail.com>");
+        .author("Victor Ndaba <vndabam@gmail.com>");
 
     program
         .add_cmd()
@@ -16,7 +16,14 @@ fn main() {
         .alias("u")
         .describe("A command to start a given app in the workspace")
         .option("-s --skip", "Skip checking/installing the dependencies")
-        .option("-p --priority", "The priority to use when starting apps")
+        .option(
+            "-a --all",
+            "Run tests for all the projects in the workspace",
+        )
+        .option(
+            "-p --priority <priority-value>",
+            "The priority to use when starting apps",
+        )
         .action(|vals, opts| up(vals, opts))
         .build(&mut program);
 
@@ -27,6 +34,24 @@ fn main() {
         .describe("A command to load directives for a given app in the workspace")
         .option("-a --all", "Load directives for all the apps")
         .action(|vals, opts| load(vals, opts))
+        .build(&mut program);
+
+    program
+        .add_cmd()
+        .command("test <app-name>")
+        .alias("t")
+        .describe("A command to run the configured tests for the provided app")
+        .option(
+            "-a --all",
+            "Run tests for all the projects in the workspace",
+        )
+        .option(
+            "-p --priority <priority-value>",
+            "Use the provided priority level to run tests.",
+        )
+        .action(|vals, opts| {
+            dbg!(vals, opts);
+        })
         .build(&mut program);
 
     program.on(Event::OutputHelp, |_p, _v| {
