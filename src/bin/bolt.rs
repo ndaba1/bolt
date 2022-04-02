6aa1dfd3e6bf8521f8304b2b66a7dc7e8ea0156c
Victor Ndaba <mwangindaba@gmail.com>
2022-04-02 23:22:52 +0300

    🔃 refactor: fixed breaking changes in cmder

--- src/bin/bolt.rs
+++ src/bin/bolt.rs
@@ -12,10 +12,9 @@ fn main() {
         .author("Victor Ndaba <vndabam@gmail.com>");
 
     program
-        .add_cmd()
         .command("up <app-name>")
         .alias("u")
-        .describe("A command to start a given app in the workspace")
+        .description("A command to start a given app in the workspace")
         .option("-s --skip", "Skip checking/installing the dependencies")
         .option(
             "-a --all",
@@ -29,28 +28,25 @@ fn main() {
         .build(&mut program);
 
     program
-        .add_cmd()
         .command("load [app-name]")
         .alias("l")
-        .describe("A command to load directives for a given app in the workspace")
+        .description("A command to load directives for a given app in the workspace")
         .option("-a --all", "Load directives for all the apps")
         .action(cmd::load)
         .build(&mut program);
 
     program
-        .add_cmd()
         .command("ci <app-name>")
         .alias("c")
-        .describe("Run A CI workflow for the provided app")
+        .description("Run A CI workflow for the provided app")
         .option("-a --all", "Run CI workflows for all apps in the workspace")
         .action(|_v, _o| {})
         .build(&mut program);
 
     program
-        .add_cmd()
         .command("test <app-name>")
         .alias("t")
-        .describe("A command to run the configured tests for the provided app")
+        .description("A command to run the configured tests for the provided app")
         .option(
             "-a --all",
             "Run tests for all the projects in the workspace",
