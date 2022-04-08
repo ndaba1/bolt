cc212a934aa152b4e3fb4784ef0e41f946c8dadf
Victor Ndaba <mwangindaba@gmail.com>
2022-04-08 22:18:33 +0300

    📦 feat: created mod for resolving bolt dependencies

---
+++ src/lib/core/deps.rs
@@ -0,0 +1,9 @@
+// /**
+//  * Check the global bolt file
+//  * get the required deps,
+//  * cargo install them
+//  * have a check env function/inject env
+//  */
+// fn load_deps() {
+//     // parse the bolt file, get deps, install them or check cache
+// }
